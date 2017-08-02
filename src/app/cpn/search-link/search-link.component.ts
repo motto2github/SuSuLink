385e41f211e0f24c2dd330daea0cb69d5a7f6ecd
motto2github <motto2email@163.com>
2017-08-02 15:36:34 +0800

    移动端链接搜索界面完善

--- src/app/cpn/search-link/search-link.component.ts
+++ src/app/cpn/search-link/search-link.component.ts
@@ -12,11 +12,14 @@ export class SearchLinkComponent implements OnInit, AfterViewInit {
 
   private keywords: FormControl = new FormControl('');
 
+  private search_histories: Array<string>;
+
   constructor(private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit() {
     this.keywords.setValue(this.route.snapshot.params.keywords);
+    this.search_histories = JSON.parse(localStorage.getItem('__ssl_search_histories')) || [];
   }
 
   ngAfterViewInit(): void {
@@ -24,7 +27,28 @@ export class SearchLinkComponent implements OnInit, AfterViewInit {
   }
 
   private onSubmit() {
+    this.keywords.setValue(this.keywords.value.trim());
+    if (this.keywords.value !== '') {
+      this.search_histories = this.search_histories.filter(item => this.keywords.value !== item);
+      this.search_histories.unshift(this.keywords.value);
+      localStorage.setItem('__ssl_search_histories', JSON.stringify(this.search_histories));
+    }
     this.router.navigate([`/home/${this.route.snapshot.params.category}/list`, this.keywords.value]);
+  }
+
+  private onHotItemClick(keywords) {
+    this.keywords.setValue(keywords);
+    this.onSubmit();
+  }
+
+  private onHistoryItemClick(keywords) {
+    this.keywords.setValue(keywords);
+    this.onSubmit();
+  }
+
+  private onClearHistoriesClick() {
+    this.search_histories = [];
+    localStorage.removeItem('__ssl_search_histories');
   }
 
 }
