c90c639fd7c4c591e69f1d0edb12a41f259e8c18
motto2github <motto2email@163.com>
2017-08-18 15:23:49 +0800

    keywords与description交换位置

--- src/app/cpn/user-link-insert/user-link-insert.component.ts
+++ src/app/cpn/user-link-insert/user-link-insert.component.ts
@@ -91,7 +91,7 @@ export class UserLinkInsertComponent implements OnInit, DoCheck, AfterViewInit {
         this.href.enable();
         if (ri.code !== 1) return this.errMsg = ri.msg;
         this.title.setValue(ri.data.title);
-        this.summary.setValue(ri.data.keywords + (ri.data.keywords && ri.data.description ? '\n\n' : '') + ri.data.description);
+        this.summary.setValue(ri.data.description + (ri.data.description && ri.data.keywords ? '\n\n' : '') + ri.data.keywords);
         this.iconUrl.setValue(ri.data.iconUrl);
       }, 150);
     });
